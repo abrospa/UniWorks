01cab6855b6769c375f0aa58f3299c427292d559
Alexander <brosspa@gmail.com>
2016-03-06 22:22:31 +0700

    TPL Update
    Added first 3 laboratory works.

---
+++ Theory of Programming Languages/Laboratory works/1/Program/1/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace _1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(13, 13);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(189, 20);
+            this.textBoxInput.TabIndex = 0;
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.Location = new System.Drawing.Point(209, 13);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(63, 20);
+            this.buttonEnter.TabIndex = 1;
+            this.buttonEnter.Text = "Ввод";
+            this.buttonEnter.UseVisualStyleBackColor = true;
+            this.buttonEnter.Click += new System.EventHandler(this.buttonEnter_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Результат:";
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labelOutput.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.labelOutput.Location = new System.Drawing.Point(256, 38);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.labelOutput.Size = new System.Drawing.Size(16, 13);
+            this.labelOutput.TabIndex = 3;
+            this.labelOutput.Text = "...";
+            this.labelOutput.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 68);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonEnter);
+            this.Controls.Add(this.textBoxInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Автомат";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button buttonEnter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelOutput;
+    }
+}
+
---
+++ Theory of Programming Languages/Laboratory works/1/Program/1/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace _1
+{
+    public partial class Form1 : Form
+    {
+        int[,] transTable = new int[30, 2];
+
+        public Form1()
+        {
+            InitializeComponent();
+            FillTransTable();
+        }
+
+        private void FillTransTable()
+        {
+            transTable[0, 0] = 1;
+            transTable[0, 1] = 13;
+            transTable[1, 0] = 2;
+            transTable[1, 1] = 6;
+            transTable[2, 0] = -1;
+            transTable[2, 1] = 3;
+            transTable[3, 0] = -1;
+            transTable[3, 1] = 4;
+            transTable[4, 0] = -1;
+            transTable[4, 1] = 5;
+            transTable[5, 0] = -1;
+            transTable[5, 1] = 5;
+            transTable[6, 0] = 7;
+            transTable[6, 1] = 9;
+            transTable[7, 0] = -1;
+            transTable[7, 1] = 8;
+            transTable[8, 0] = -1;
+            transTable[8, 1] = 5;
+            transTable[9, 0] = 10;
+            transTable[9, 1] = 11;
+            transTable[10, 0] = -1;
+            transTable[10, 1] = 5;
+            transTable[11, 0] = 12;
+            transTable[11, 1] = 11;
+            transTable[12, 0] = -1;
+            transTable[12, 1] = 5;
+            transTable[13, 0] = 14;
+            transTable[13, 1] = 21;
+            transTable[14, 0] = 15;
+            transTable[14, 1] = 17;
+            transTable[15, 0] = -1;
+            transTable[15, 1] = 16;
+            transTable[16, 0] = -1;
+            transTable[16, 1] = 5;
+            transTable[17, 0] = 18;
+            transTable[17, 1] = 19;
+            transTable[18, 0] = -1;
+            transTable[18, 1] = 5;
+            transTable[19, 0] = 20;
+            transTable[19, 1] = 19;
+            transTable[20, 0] = -1;
+            transTable[20, 1] = 5;
+            transTable[21, 0] = 22;
+            transTable[21, 1] = 26;
+            transTable[22, 0] = 23;
+            transTable[22, 1] = 24;
+            transTable[23, 0] = -1;
+            transTable[23, 1] = 5;
+            transTable[24, 0] = 25;
+            transTable[24, 1] = 24;
+            transTable[25, 0] = -1;
+            transTable[25, 1] = 5;
+            transTable[26, 0] = 27;
+            transTable[26, 1] = 26;
+            transTable[27, 0] = 28;
+            transTable[27, 1] = 29;
+            transTable[28, 0] = -1;
+            transTable[28, 1] = 5;
+            transTable[29, 0] = 28;
+            transTable[29, 1] = 29;
+        }
+
+        private bool IsFinalState(int stateNumber)
+        {
+            bool value = false;
+            switch (stateNumber)
+            {
+                case 5:
+                    return true;
+                case 12:
+                    return true;
+                case 20:
+                    return true;
+                case 28:
+                    return true;
+                default:
+                    break;
+            }
+            return value;
+        }
+
+        private bool IsAccepted(string text)
+        {
+            int currentState = 0;
+            bool finalState = false;
+
+            foreach (char symbol in text)
+            {
+                if (symbol == 'a')
+                    currentState = transTable[currentState, 0];
+                else if (symbol == 'b')
+                    currentState = transTable[currentState, 1];
+                else return 
+                    false;
+
+                if (currentState == -1)
+                    return false;
+            }
+
+            if (IsFinalState(currentState))
+                return true;
+            return false;
+        }        
+
+        private void buttonEnter_Click(object sender, EventArgs e)
+        {
+            string text = textBoxInput.Text;
+            if (IsAccepted(text))
+            {
+                labelOutput.Location = new Point(186, 40);
+                labelOutput.ForeColor = Color.Green;
+                labelOutput.Text = "Строка принята.";
+            }
+            else
+            {
+                labelOutput.Location = new Point(171, 40);
+                labelOutput.ForeColor = Color.Red;
+                labelOutput.Text = "Строка отвергнута.";
+            }
+        }
+    }
+}
